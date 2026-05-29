eeec4cf9700018923edfe774b9c05e2076e2a2d1
AlejandraM1515 <a01714499@tec.mx>
2026-05-28 19:23:02 -0600

    Add files via upload

---
+++ Tercer avance/cliente_vip.h
@@ -0,0 +1,30 @@
+#ifndef CLIENTE_VIP_H
+#define CLIENTE_VIP_H
+#include <string>
+#include "cliente.h"
+using namespace std;
+
+/*Clase Cliente VIP*/
+class Cliente_vip : public Cliente {
+private:
+    string beneficios;
+public:
+    //Metodo constructor
+    Cliente_vip();
+    Cliente_vip(int, string, string, int, string);
+    //Get y Set
+    void setBeneficios(string);
+    string getBeneficios();
+    //otros metodos
+    //Polimorfismo
+    virtual void info_cliente();
+    virtual void mostrar_precio();
+    virtual float get_precio_unitario();
+
+    
+    double calcular_descuento();
+    void precio_boleto_descuento();
+    void precio_boleto_descuento(int cantidad); //Sobrecarga del metodo: Recibe la cantidad de boletos uqe se desea comprar para mostrar la cliente cunato pagara aplicando el descuento
+};
+
+#endif // CLIENTE_VIP_H
