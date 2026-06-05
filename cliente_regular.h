a6728e84cd338c2c36fc54dc7e23a1428cd92a8e
AlejandraM1515 <a01714499@tec.mx>
2026-06-05 10:13:34 -0600

    Add files via upload

---
+++ cliente_regular.h
@@ -0,0 +1,27 @@
+#ifndef CLIENTE_REGULAR_H
+#define CLIENTE_REGULAR_H
+#include <string>
+#include "cliente.h"
+using namespace std;
+
+/*Clase Cliente regular*/
+class Cliente_regular : public Cliente {
+private:
+    int puntos_fidelidad;
+public:
+    //Metodo constructor
+    Cliente_regular();
+    Cliente_regular(string, string, int, int);
+    //Get y Set
+    void setPuntos_fidelidad(int);
+    int getPuntos_fidelidad();
+    //otros metodos
+    virtual void info_cliente(); //Polimorfismo
+    virtual void mostrar_precio();
+    virtual float get_precio_unitario();
+
+    void precio_boleto();
+    void precio_boleto(int cantidad); // Sobrecarga del metodo: Recibe la cantidad de boletos que se desea comprar para mostrar la cliente cunato pagara
+};
+
+#endif // CLIENTE_REGULAR_H
